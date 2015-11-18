cef3af1d3c4efcc007842369fd1e6a79e34ae778
Joshua Kephart <jak243@pitt.edu>
2015-11-18 10:04:57 -0500

    heyoo

--- methods.py
+++ methods.py
@@ -60,3 +60,52 @@ def generalSystem3d(S,t,G=6.67408e-11):
         return_array = np.append(return_array,[dm[i],vx[i],vy[i],vz[i],dvx[i],dvy[i],dvz[i]])
     
     return return_array
+	
+def generalSystem2d(S,t,G=6.67408e-11):
+    #S0 includes m formatted mi,xi,yi,zi,vxi,vyi,vzi
+    #G=6.67408e-11
+    #print(S0)
+    N=len(S)//5
+    x = np.zeros(N)
+    y = np.zeros(N)
+    vx = np.zeros(N)
+    vy = np.zeros(N)
+    m = np.zeros(N)
+    
+    xr3 = np.zeros((N,N))
+    yr3 = np.zeros((N,N))
+    
+    dvx = np.zeros(N)
+    dvy = np.zeros(N)
+    
+    dm = np.zeros(N)
+    
+    for i in range(N):
+        m[i] = S[6*i]
+        x[i] = S[6*i+1]
+        y[i] = S[6*i+2]
+        vx[i] = S[6*i+3]
+        vy[i] = S[6*i+4]
+    
+    
+    for i in range(N):
+        for j in range(N):
+            if(i!=j):
+                r = np.sqrt((x[i]-x[j])**2+(y[i]-y[j])**2)
+                xr3[i][j] = (x[i]-x[j])/(r**3)
+                yr3[i][j] = (y[i]-y[j])/(r**3)
+
+            
+    for i in range(N):
+        for j in range(N):
+            if(i!=j):
+                dvx[i] = dvx[i]-G*m[j]*xr3[i][j]
+                dvy[i] = dvy[i]-G*m[j]*yr3[i][j]
+    
+    
+    return_array = np.array([])
+    
+    for i in range(N):
+        return_array = np.append(return_array,[dm[i],vx[i],vy[i],dvx[i],dvy[i]])
+    
+    return return_array
